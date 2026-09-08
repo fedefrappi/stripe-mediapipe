aa64e84fccebde0d5f650f2df48f1b04e0cf6d01
stripe-ci <support+github@stripe.com>
2026-09-08 12:41:23 +0000

    Release 1.0.0

--- Package.swift
+++ Package.swift
@@ -51,17 +51,17 @@ let package = Package(
         .binaryTarget(
             name: "MediaPipeTasksVision",
             url: "\(assets)/MediaPipeTasksVision.xcframework.zip",
-            checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+            checksum: "8c1038dd24d8b0e72dfc4a7891f7f4367e53484345d223d35e85797859b9e961"
         ),
         .binaryTarget(
             name: "MediaPipeCommonGraphLibraries",
             url: "\(assets)/MediaPipeCommonGraphLibraries.xcframework.zip",
-            checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+            checksum: "09d55d7dc4fe74c571fdc3650fd90513de19fdaea033e5f8dc9a7cae712fd411"
         ),
         .binaryTarget(
             name: "MediaPipeTasksCommon",
             url: "\(assets)/MediaPipeTasksCommon.xcframework.zip",
-            checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+            checksum: "786e8d8768963b807b73ff158033cfa2d796dcb82a5854b794b7285742b8fe78"
         ),
     ]
 )
